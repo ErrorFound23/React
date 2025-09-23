0e966a8de24f4554b614572c52aeacf24531e617
ErrorFound23 <ranayash35495@gmail.com>
2025-09-23 18:04:59 +0530

    react with tailwindcss

---
+++ 03_tailwindprops/src/App.jsx
@@ -0,0 +1,15 @@
+import {} from "react";
+import Card from "./components/card";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <Card channel="chaiorcode" lblPlatform="youtube.com" />
+      <Card channel="apnacollege" />
+    </>
+  );
+}
+
+export default App;
---
+++ 03_tailwindprops/src/components/Card.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Card = ({channel, lblPlatform="linkedin.com"}) => {
+    console.log(channel);
+    
+  return (
+    <div className="max-w-xs p-6 rounded-md shadow-md bg-black mb-4">
+      <img
+        src="https://media1.giphy.com/media/z8n8dWgQ0mgEIyzlmV/giphy.gif?cid=790b7611a5ba988db1bc7457636dd163c28af6f6dbc84a77&rid=giphy.gif&ct=g"
+        alt=""
+        className="object-cover object-center w-full rounded-md h-72 bg-gray-500"
+      />
+      <div className="mt-6 mb-2">
+        <span className="block text-sm font-medium font-mono tracking-widest uppercase text-indigo-400">
+          {channel}
+        </span>
+        <h2 className="text-xl font-semibold tracking-wide">
+          {lblPlatform}
+        </h2>
+      </div>
+      <p className="text-gray-300">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
+        tempora ipsum soluta amet
+      </p>
+    </div>
+  );
+};
+
+export default Card;
